ed74fcae75da3515ab1d3be29fe280547af35f30
Maxim <119050056+Ambaccador-Gre4ki@users.noreply.github.com>
2026-05-25 15:39:01 +0800

    Create HTTPS.cs
    Создал класс для подключения по сертификату

---
+++ FinMonAPI/HTTPS.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FinMonAPI
+{
+    public class Client
+    {
+        public HttpClient InitClient()
+        {
+            string myCertThumbprint = "ЗАМЕНИТЬ НА ОТПЕЧАТОК СЕРТИФИКАТА";
+
+            // Достаем сертификат из системы
+            var gostCert = CertificateProvider.GetGostCertificate(myCertThumbprint);
+
+            // Инициализируем handler и прикрепляем сертификат
+            var handler = new HttpClientHandler();
+            handler.ClientCertificates.Add(gostCert);
+
+            // Создаем клиент для работы
+            return new HttpClient(handler);
+        }
+    }
+}
