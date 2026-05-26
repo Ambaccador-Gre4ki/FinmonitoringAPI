1089e56065ff5c71267270f1a84a6481b47891f9
Maxim <119050056+Ambaccador-Gre4ki@users.noreply.github.com>
2026-05-26 15:35:20 +0800

    Create logs
    Добавил пакет для логгирования

--- FinMonAPI/Program.cs
+++ FinMonAPI/Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Serilog;
 
 namespace FinMonAPI
 {
@@ -12,6 +13,16 @@ namespace FinMonAPI
         static async Task Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug() // Записываем Debug, Information, Warning, Error
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .WriteTo.File(
+                    path: Path.Combine(AppContext.BaseDirectory, "logs", "log-.txt"),
+                    rollingInterval: RollingInterval.Day, // Автоматический новый файл каждый день
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+
+            Log.Information("Приложение FinMonAPI успешно запущено.");
 
             // НАСТРОЙКИ ДЛЯ ПОДКЛЮЧЕНИЯ
             string thumbprint = "********";
@@ -20,10 +31,13 @@ namespace FinMonAPI
 
             try
             {
+                Log.Information("=== Поиск сертификата ===");
                 Console.WriteLine("Шаг 1: Поиск ГОСТ-сертификата...");
                 var cert = CertificateProvider.GetGostCertificate(thumbprint);
                 Console.WriteLine($"Сертификат найден: {cert.Subject}\n");
+                Log.Information("Сертификат успешно подключен: {Subject}", cert.Subject);
 
+                Log.Information("=== Инициализация соединения ===");
                 Console.WriteLine("Шаг 2: Инициализация HTTPS-соединения...");
                 var handler = new HttpClientHandler();
                 handler.ClientCertificates.Add(cert);
@@ -38,39 +52,56 @@ namespace FinMonAPI
                 httpClient.Timeout = TimeSpan.FromMinutes(10); // Защита от таймаутов на больших файлах
 
                 // Авторизация и получение JWT-токена
+                Log.Information("=== Запрос токена ===");
                 Console.WriteLine("Шаг 3: Запрос JWT-токена сессии...");
                 var authService = new AuthService(httpClient);
                 string accessToken = await authService.AuthenticateAsync(login, password);
                 Console.WriteLine("Токен успешно получен!");
+                Log.Information("Токен получен");
 
                 // Ставим Bearer-токен в заголовки для всех последующих запросов
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
                 // Работа с каталогами справочников
+                Log.Information("=== Скачивание справочников ===");
                 var catalogService = new CatalogService(httpClient);
                 Console.WriteLine("\nШаг 4: Скачивание актуальных перечней...");
 
                 // --- 4.1 Скачивание Перечня Террористов (TE2) ---
+                Log.Debug("/// Запрос актуального списка экстремистов ///");
                 var te2Catalog = await catalogService.GetTe2CatalogAsync();
                 if (te2Catalog != null && te2Catalog.IsActive)
                 {
                     string path = Path.Combine(AppContext.BaseDirectory, "current_te2.zip");
+                    Log.Information("Найден активный перечень ТЭ от {Date}. Скачивание файла...", te2Catalog.Date);
                     Console.WriteLine($"-> Найден активный перечень ТЭ от {te2Catalog.Date}. Скачивание...");
                     await catalogService.DownloadTe2FileAsync(te2Catalog.IdXml, path);
                     Console.WriteLine($"   Файл ТЭ сохранен: {path}");
+                    Log.Information("Файл ТЭ успешно сохранен: {Path}", path);
                 }
-
+                else
+                {
+                    Log.Warning("!!! Актуальный перечень ТЭ не найден или неактивен !!!");
+                }
                 //// --- 4.2 Скачивание Русской версии Перечня ООН ---
+                //Log.Debug("/// Запрос актуального каталога ООН (RU) ///");
                 //var unRusCatalog = await catalogService.GetUnCatalogRusAsync();
                 //if (unRusCatalog != null && unRusCatalog.IsActive)
                 //{
                 //    string path = Path.Combine(AppContext.BaseDirectory, "current_un_rus.xml");
+                //    Log.Information("Найден активный перечень ООН (RU) от {Date}. Скачивание файла...", unRusCatalog.Date);
                 //    Console.WriteLine($"-> Найден активный перечень ООН (RU) от {unRusCatalog.Date}. Скачивание...");
                 //    await catalogService.DownloadUnFileAsync(unRusCatalog.IdXml, path);
                 //    Console.WriteLine($"   Файл ООН (RU) сохранен: {path}");
+                //    Log.Information("Файл ООН (RU) успешно сохранен: {Path}", path);
+                //}
+                //else
+                //{
+                //    Log.Warning("!!! Актуальный перечень ООН (RU) не найден или неактивен !!!");
                 //}
 
                 Console.WriteLine("\nВсе операции успешно завершены!");
+                Log.Information("Все запланированные операции успешно завершены.");
             }
             catch (Exception ex)
             {
@@ -78,9 +109,14 @@ namespace FinMonAPI
                 Console.WriteLine($"\nПроизошла критическая ошибка:");
                 Console.WriteLine(ex.Message);
                 if (ex.InnerException != null) Console.WriteLine($"Детали: {ex.InnerException.Message}");
+                Log.Fatal(ex, "### Произошел критический сбой при выполнении программы ###");
                 Console.ResetColor();
             }
-
+            finally
+            {
+                Log.Information("Приложение завершает свою работу.");
+                Log.CloseAndFlush(); // Принудительно сбрасываем буфер логов в файл на диске
+            }
             Console.WriteLine("\nНажмите любую клавишу для выхода...");
             Console.ReadKey();
         }
